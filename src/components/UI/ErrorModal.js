483eccdb6792435f5ee4cbfe8580297dab2938ec
paalss <paalss94@gmail.com>
2021-04-21 10:35:09 +0200

    bruker react-dom's createPortal til å rendre ErrorModal (error popup) like under <body>. Dette er et element som vises over hele skjermen, så det gir mening at det skal rendres der og ikke dybt inne i html-strukturen. bedre accessibility.

--- src/components/UI/ErrorModal.js
+++ src/components/UI/ErrorModal.js
@@ -2,22 +2,39 @@ import Card from "./Card";
 import Button from "./Button";
 import styles from "./ErrorModal.module.css";
 import { Fragment } from "react";
+import { createPortal } from "react-dom";
+
+const Backdrop = ({ onConfirm }) => {
+  return <div className={styles.backdrop} onClick={onConfirm} />;
+};
+
+const ModalOverlay = ({ title, message, onConfirm }) => {
+  return (
+    <Card className={styles.modal}>
+      <header className={styles.header}>
+        <h2>{title}</h2>
+      </header>
+      <div className={styles.content}>
+        <p>{message}</p>
+      </div>
+      <footer className={styles.actions}>
+        <Button onClick={onConfirm}>OK</Button>
+      </footer>
+    </Card>
+  );
+};
 
 const ErrorModal = ({ title, message, onConfirm }) => {
   return (
     <Fragment>
-      <div className={styles.backdrop} onClick={onConfirm} />
-      <Card className={styles.modal}>
-        <header className={styles.header}>
-          <h2>{title}</h2>
-        </header>
-        <div className={styles.content}>
-          <p>{message}</p>
-        </div>
-        <footer className={styles.actions}>
-          <Button onClick={onConfirm}>OK</Button>
-        </footer>
-      </Card>
+      {createPortal(
+        <Backdrop onConfirm={onConfirm} />,
+        document.getElementById("backdrop-root")
+      )}
+      {createPortal(
+        <ModalOverlay title={title} message={message} onConfirm={onConfirm} />,
+        document.getElementById("overlay-root")
+      )}
     </Fragment>
   );
 };
